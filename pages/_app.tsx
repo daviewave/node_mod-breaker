a56ab731196644729260819bdc3e0b76049215a4
David Silveira <daviewave@gmail.com>
2021-11-05 12:22:13 -0400

    added files

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+
+// import $ from "jquery";
+
+require("../styles/globals.css");
+require("../styles/solana.css");
+
+//bootstrap import
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/navbar-override.css";
+
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
+import { FC, ReactNode } from "react";
+import { AppProps } from "next/app";
+
+const WalletConnectionProvider = dynamic<{ children: ReactNode }>(
+  () =>
+    import("../components/walletConnectionProvider").then(
+      ({ WalletConnectionProvider }) => WalletConnectionProvider
+    ),
+  {
+    ssr: false,
+  }
+);
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <WalletConnectionProvider>
+      <WalletModalProvider>
+        <Component {...pageProps} />
+      </WalletModalProvider>
+    </WalletConnectionProvider>
+  );
+};
+
+export default MyApp;
